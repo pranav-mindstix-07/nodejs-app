52f1208ebb71c7010569ef35eedb6d29e1688b15
Pranav More <pranav.more@mindstix.com>
2024-07-20 15:50:15 +0530

    Updated

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 app.get('/', (req, res) => {
-    res.status(200).json({message : 'Hello World, from express'});
+    res.status(200).json({message : 'Hello World, from express okk'});
 })
 
 app.post("/adduser", async (req, res) => {
